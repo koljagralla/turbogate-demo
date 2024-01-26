f92734577cf571b83ea44e3cea844f42b6093c9c
Kolja Gralla <mail@koljagralla.de>
2024-01-26 16:39:01 +0100

    added prettier and formatted whole repo

--- lib/stacks/my-stack/index.ts
+++ lib/stacks/my-stack/index.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyStack extends cdk.Stack {
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-		super(scope, id, props);
-	}
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+  }
 }
