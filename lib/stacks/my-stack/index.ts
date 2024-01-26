09dc88c21e6e4744661dc8703477cbd7d7a72579
Kolja Gralla <mail@koljagralla.de>
2024-01-26 17:59:58 +0100

    added MyApiTurbogate to IaC code

--- lib/stacks/my-stack/index.ts
+++ lib/stacks/my-stack/index.ts
@@ -1,11 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { createItemsDynamoDB } from './database/items/create-items-dynamodb';
+import { MyApiTurbogate } from './my-api-turbogate/my-api-turbogate';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const itemsTable = createItemsDynamoDB(this);
+
+    const myApiTurbogate = new MyApiTurbogate(this, {
+      environment: {
+        ITEMS_TABLE_NAME: itemsTable.tableName,
+        VALID_API_KEYS: 'my-api-key,another-api-key',
+      },
+      permissions: {
+        ITEMS_TABLE_WRITE: lambda => itemsTable.grantWriteData(lambda),
+      },
+    });
   }
 }
