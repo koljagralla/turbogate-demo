011fd647cbcdc87d4a689cce4ceee1d14848fe94
Kolja Gralla <mail@koljagralla.de>
2024-01-26 16:46:01 +0100

    added deps, changed to tsx, wired turbogate

--- bin/turbogate-demo.ts
+++ bin/turbogate-demo.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+import { wireTurbogate } from 'turbogate/local'; // Keep at top of imports!
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { MyStack } from '../lib/stacks/my-stack';
+
+wireTurbogate; // Do not remove
 
 const app = new cdk.App();
 new MyStack(app, 'TurbogateDemoStack', {});
